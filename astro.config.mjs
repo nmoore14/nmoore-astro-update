739c511c1c84b3f7321578bc60cf734d1dd5e9f5
Nick Moore <nickmoore_1@live.com>
2023-09-21 12:06:28 -0400

    Adding svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [
-    mdx({
-      syntaxHighlight: 'shiki',
-      shikiConfig: { theme: 'github-dark-dimmed' },
-    }),
-    sitemap()
-  ],
+  site: 'https://example.com',
+  integrations: [mdx({
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'github-dark-dimmed'
+    }
+  }), sitemap(), svelte()]
 });
